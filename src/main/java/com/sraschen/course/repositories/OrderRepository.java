9abc88014793049c40531ef3e671e224c0dc4999
Samuel Raschen <sraschen@yahoo.com.br>
2022-12-24 11:35:32 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/sraschen/course/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.sraschen.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sraschen.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+		
+}
