19d436720434c305ad8b22af9f424293d52c4dbb
Samuel Raschen <sraschen@yahoo.com.br>
2023-01-24 09:30:15 -0300

    Payment, one-to-one association

--- src/main/java/com/sraschen/course/config/TestConfig.java
+++ src/main/java/com/sraschen/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.sraschen.course.entities.Category;
 import com.sraschen.course.entities.Order;
 import com.sraschen.course.entities.OrderItem;
+import com.sraschen.course.entities.Payment;
 import com.sraschen.course.entities.Product;
 import com.sraschen.course.entities.User;
 import com.sraschen.course.entities.enums.OrderStatus;
@@ -80,6 +81,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+		
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 	
--- src/main/java/com/sraschen/course/entities/Order.java
+++ src/main/java/com/sraschen/course/entities/Order.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.sraschen.course.entities.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -38,6 +40,10 @@ public class Order implements Serializable {
 
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order",cascade = CascadeType.ALL) //ordem e pagamento com o mesmo Id
+	private Payment payment; 
+	
 	
 	public Order() {
 	}
@@ -91,6 +97,16 @@ public class Order implements Serializable {
 		return items;
 	}
 	
+	
+	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
