d52fc16e46f28190a81917b3f258cd217eae7b55
Samuel Raschen <sraschen@yahoo.com.br>
2023-01-26 16:07:16 -0300

    User update

--- src/main/java/com/sraschen/course/services/UserService.java
+++ src/main/java/com/sraschen/course/services/UserService.java
@@ -32,4 +32,15 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User update(Long id , User obj) {
+		User entity = repository.getReferenceById(id); //não pega o usuário do banco de dados, mas uma "cópia"
+		updateData(entity , obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
