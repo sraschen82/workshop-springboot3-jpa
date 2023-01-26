6e7e76b2737575a9f20c6fb77792c988d5fe57ae
Samuel Raschen <sraschen@yahoo.com.br>
2023-01-26 17:00:33 -0300

    Exception handling - findById

--- src/main/java/com/sraschen/course/services/UserService.java
+++ src/main/java/com/sraschen/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.sraschen.course.entities.User;
 import com.sraschen.course.repositories.UserRepository;
+import com.sraschen.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
